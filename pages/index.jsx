209be07988d0412877e30ffaadfd5de7551df905
Fred <alcide.fred@gmail.com>
2021-11-09 18:15:54 +0100

    Initialise le template pour les projets Next.js

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import Layout from '../components/layout'
+
+import utilStyles from '../styles/utils.module.css'
+
+const Home = () => {
+  return (
+    <Layout home>
+    </Layout>
+  )
+}
+
+export default Home
